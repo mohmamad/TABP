4e22ef593aee40c76075ed24ebf9f1733499f007
Mohammad Moghrabi <mohamad.isa.moghrabi@gmail.com>
2024-03-28 05:40:17 +0200

    Revert "Revert "Add invoice email service.""
    This reverts commit 931d5eac74db93d96f7336ed53b3534a0b7170b3.

---
+++ src/TABP.Domain/Interfaces/IInvoiceEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace TABP.Domain.Interfaces
+{
+    public interface IInvoiceEmailService
+    {
+        public Task SendInvoiceEmail(string recieverEmail, string recieverName, string subject, string message);
+        public Task prepareEmailMessage(string userName, string email, List<double> pricePerDay, List<int> roomNumber, List<string> hotelName, List<int> NumberOfDays);
+    }
+}
--- src/TABP.Infrastructure/Repositories/UserRepository.cs
+++ src/TABP.Infrastructure/Repositories/UserRepository.cs
@@ -83,6 +83,9 @@ namespace TABP.Infrastructure.Repositories
             }
         }
 
-
+        public async Task<User> GetUserByIdAsync(Guid userId)
+        {
+            return await _dbContext.Users.FirstOrDefaultAsync(u => u.UserId.Equals(userId));    
+        }
     }
 }
