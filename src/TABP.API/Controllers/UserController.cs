1fb9bac4ddd5b70a0a29ea0740aa0096be5c4187
Mohamad moghrabi <mohamad.isa.moghrabi@gmail.com>
2024-06-02 03:36:57 +0300

    Add database entity for password reset codes.

--- src/TABP.API/Controllers/UserController.cs
+++ src/TABP.API/Controllers/UserController.cs
@@ -118,5 +118,6 @@ namespace TABP.API.Controllers
 
         }
 
+
     }
 }
---
+++ src/TABP.Domain/Entities/ResetPasswordCode.cs
@@ -0,0 +1,10 @@
+﻿namespace TABP.Domain.Entities
+{
+    public class ResetPasswordCode
+    {
+        public int Id { get; set; }
+        public string code { get; set; }
+        public Guid UserId { get; set; }
+        public User user { get; set; }
+    }
+}
